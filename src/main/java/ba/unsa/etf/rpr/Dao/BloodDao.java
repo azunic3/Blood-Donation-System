c573e43da1669c5f91207634c1d1a0ff367633c0
azunic3 <azunic3@etf.unsa.ba>
2022-12-11 13:13:30 +0100

    interface for blood domain bean

---
+++ src/main/java/ba/unsa/etf/rpr/Dao/BloodDao.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.Dao;
+
+import ba.unsa.etf.rpr.Domain.Blood;
+
+/**
+ * DAO interface for Blood domain bean
+ * @author Azra Žunić
+ * version 1.0
+ */
+public interface BloodDao extends Dao<Blood> {
+}
