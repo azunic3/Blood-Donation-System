c60cc34bc064f044f4a48cf580bf82a9bf43e1c4
azunic3 <azunic3@etf.unsa.ba>
2022-12-12 21:02:45 +0100

    login window added(basic commands)

--- src/main/java/ba/unsa/etf/rpr/MainFX.java
+++ src/main/java/ba/unsa/etf/rpr/MainFX.java
@@ -4,7 +4,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class MainFX extends Application {
     public void start(Stage stage) throws Exception {
@@ -13,9 +17,16 @@ public class MainFX extends Application {
         fl.setController(controller);
         Parent root = null;
         root = fl.load();
-        //stage.setTitle("Hello, World!");
-        //stage.setScene(new Scene(root, 300, 275));
-        //stage.show();
+        stage.setTitle("Account information");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE)); //da bude taman veličine prozor
+        //osim toga, padding za GridPane treba postaviti da ne bude zalijepljeno za ivice, hgap i vgap za razmak izmedju redova i kolona
+        //velicina prozora je po defaultu resizable, a ne treba biti za dijaloske prozore, moramo to mijenjati kroz kod:
+        //stage.setResizable(false);
+        stage.setMinHeight(120); //postavimo minimalnu velicinu prozora
+        stage.setMinWidth(200);
+        //Hgrow na always da s promjenom velicine prozora raste i text field i button
+        stage.show();
+
     }
     public static void main(String[] args) {
         launch(args);
