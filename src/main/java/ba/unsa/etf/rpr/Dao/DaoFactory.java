12f5015ae1ca4ca676f3530248978cbf817d2274
azunic3 <azunic3@etf.unsa.ba>
2022-12-16 22:53:24 +0100

    factory method added

---
+++ src/main/java/ba/unsa/etf/rpr/Dao/DaoFactory.java
@@ -0,0 +1,36 @@
+package ba.unsa.etf.rpr.Dao;
+
+
+import ba.unsa.etf.rpr.Domain.Hospital;
+
+/**
+ * Factory method for singleton implementation of DAOs
+ *
+ * @author Azra Žunić
+ */
+public class DaoFactory {
+    private static final BloodDao bloodDao = new BloodDaoSQLImpl();
+    private static final DonorDao donorDao = new DonorDaoSQLImpl();
+    private static final PatientDao patientDao = new PatientDaoSQLImpl();
+    private static final HospitalDao hospitalDao = new HospitalDaoSQLImpl();
+
+    private DaoFactory(){
+    }
+
+    public static BloodDao bloodDao(){
+        return bloodDao;
+    }
+
+    public static DonorDao donorDao(){
+        return donorDao;
+    }
+
+    public static PatientDao patientDao(){
+        return patientDao;
+    }
+    public static HospitalDao hospitalDao(){
+        return hospitalDao;
+    }
+
+
+}
