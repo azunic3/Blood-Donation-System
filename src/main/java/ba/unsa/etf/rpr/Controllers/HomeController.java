6ad33e4f83690562d0d33cf170929b3d425120bd
azunic3 <azunic3@etf.unsa.ba>
2022-12-24 21:56:04 +0100

    home controller like the one on github

--- src/main/java/ba/unsa/etf/rpr/Controllers/HomeController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/HomeController.java
@@ -1,4 +1,4 @@
-package ba.unsa.etf.rpr.controllers;
+package ba.unsa.etf.rpr.Controllers;
 import ba.unsa.etf.rpr.Dao.BloodDaoSQLImpl;
 import ba.unsa.etf.rpr.Dao.DonorDaoSQLImpl;
 import ba.unsa.etf.rpr.Dao.HospitalDaoSQLImpl;
@@ -10,21 +10,9 @@ import ba.unsa.etf.rpr.Domain.Patient;
 import ba.unsa.etf.rpr.exceptions.BloodException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-
-import java.io.IOException;
-import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class HomeController {
     public ListView<Blood> bloodListView;
