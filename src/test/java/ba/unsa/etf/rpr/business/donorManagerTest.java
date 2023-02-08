076f9538afc3d9286a8a57f3caa08df4dde1c0bd
azunic3 <azunic3@etf.unsa.ba>
2023-02-08 21:15:04 +0100

    minor changes in testing classes

--- src/test/java/ba/unsa/etf/rpr/business/donorManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/business/donorManagerTest.java
@@ -1,25 +1,17 @@
 package ba.unsa.etf.rpr.business;
-
-import ba.unsa.etf.rpr.Dao.DaoFactory;
 import ba.unsa.etf.rpr.Dao.DonorDaoSQLImpl;
-import ba.unsa.etf.rpr.Domain.Blood;
 import ba.unsa.etf.rpr.Domain.Donor;
-import ba.unsa.etf.rpr.Domain.Hospital;
 import ba.unsa.etf.rpr.Exceptions.BloodException;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
 import org.mockito.Mockito;
-
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.*;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * @author Azra Žunić
@@ -78,25 +70,7 @@ public class donorManagerTest {
         String donated = "YES";
         Donor dd=new Donor();
         dd.setAlreadyDonated(donated);
-//        dd.setId(10);
         dd.setFullName("Lejla Jakupovic");
-//        dd.setPassword("lejlaj");
-//        dd.setAlreadyDonated("YES");
-//        Calendar.getInstance().set(2002,3,14);
-//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-//        Date d=Calendar.getInstance().getTime();
-//        String d1= sdf.format(d);
-//        d=sdf.parse(d1);
-//        dd.setDateOfBirth(d);
-//        dd.setPhoneNumber(62830333);
-//        Hospital hod=new Hospital();
-//        hod.setId(2);
-//        dd.setFk_Hospital(hod);
-//        dd.setGender("F");
-//        Blood bl = new Blood();
-//        bl.setId(2);
-//        dd.setBloodType_id_fk(bl);
         assertEquals(dd.getFullName(),donorManager.searchByDonorsName("Lejla Jakupovic").getFullName(),"Something is wrong");
-
     }
     }
--- src/test/java/ba/unsa/etf/rpr/business/patientManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/business/patientManagerTest.java
@@ -1,21 +1,16 @@
 package ba.unsa.etf.rpr.business;
-
 import ba.unsa.etf.rpr.Dao.DaoFactory;
 import ba.unsa.etf.rpr.Dao.PatientDaoSQLImpl;
-import ba.unsa.etf.rpr.Domain.Blood;
 import ba.unsa.etf.rpr.Domain.Patient;
 import ba.unsa.etf.rpr.Exceptions.BloodException;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
