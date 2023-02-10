d9d040f6b724c199b4236748316d1e241546c762
azunic3 <azunic3@etf.unsa.ba>
2023-02-10 21:45:38 +0100

    changing CLI

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr;
 import ba.unsa.etf.rpr.Domain.Blood;
+import ba.unsa.etf.rpr.Domain.Hospital;
 import ba.unsa.etf.rpr.Domain.Patient;
 import ba.unsa.etf.rpr.business.BloodManager;
 import ba.unsa.etf.rpr.business.PatientManager;
@@ -10,22 +11,25 @@ import java.util.List;
 
 /**
  * @author Azra Žunić
- * version 1.2
- *  CLI (Command Line Interface) implementation in following class
+ * final version 1.3
+ * CLI (Command Line Interface) implementation in following class
  */
 public class App {
     /**
      * Defining final variables to describe all code having options
      */
-    private static final Option addPatient = new Option("p", "add-patient", false, "Adding new patient to BloodDonationSystem database");
-    private static final Option addBlood = new Option("b", "add-blood type", false, "Adding new blood group to BloodDonationSystem database");
-    private static final Option getPatient = new Option("getP", "get-patient", false, "Printing all patients from BloodDonationSystem database");
-    private static final Option getBlood = new Option("getB", "get-blood type", false, "Printing all blood groups from BloodDonationSystem database");
-    private static final Option bloodtype = new Option(null, "BloodType", false, "Defining blood group for next added patient");
+    private static final Option addHospital = new Option("addH", "add-hospital", false, "Adding new hospital to database");
+    private static final Option getHospital = new Option("getH", "get-hospital", false, "Printing all hospitals from database");
+    private static final Option getBlood = new Option("getB", "get-blood", false, "Printing all attributes of table Blood");
+    private static final Option updateHospital = new Option("uD", "update", false, "Updating information about hospital");
+    private static final Option deleteHospital = new Option("d", "delete", false, "Deleting hospital");
+    private static final Option name = new Option("n", "name", false, "Defining name of the hospital");
+    private static final Option address = new Option("ad", "address", false, "Defining the address of the hospital");
+    private static final Option contactNumber = new Option("cn", "contact-number", false, "Defining the contact number of an institution");
+    private static final Option hospID = new Option("hId", "hospitalfk", false, "Defining the hospital where blood is stored");
+    private static final Option quantityOnHand = new Option("q", "quantity-on-hand", false, "Defining available quantity of blood");
+    private static final Option bloodBagNumber = new Option("ba", "blood-bag-number", false, "Defining blood bag number");
 
-    /**
-     * @param options
-     */
     public static void printFormattedOptions(Options options) {
         HelpFormatter helpFormatter = new HelpFormatter();
         PrintWriter printWriter = new PrintWriter(System.out);
@@ -33,79 +37,27 @@ public class App {
         helpFormatter.printOptions(printWriter, 150, options, 2, 7);
         printWriter.close();
     }
-public static Options addOptions() {
+
+    public static Options addOptions() {
         Options options = new Options();
-        options.addOption(addPatient);
-        options.addOption(addBlood);
-        options.addOption(getPatient);
-        options.addOption(getBlood);
-        options.addOption(bloodtype);
+        options.addOption(addHospital);
+        options.addOption(getHospital );
+        options.addOption(getBlood );
+        options.addOption(updateHospital);
+        options.addOption(deleteHospital);
+        options.addOption(name);
+        options.addOption(address);
+        options.addOption(contactNumber);
+        options.addOption(hospID);
+        options.addOption(quantityOnHand);
+        options.addOption(bloodBagNumber);
         return options;
     }
-
-    public static Blood searchThroughBloodTypes(List<Blood> listOfBlodTypes, String BloodGroup) {
-        Blood blood = null;
-        blood = listOfBlodTypes.stream().filter(p -> p.getBloodGroup().equals(BloodGroup)).findAny().get();
-        return blood;
+    public static Hospital searchThroughHospitals(List<Hospital> listOfHosp, String eName) {
+        Hospital e = listOfHosp.stream().filter(p -> p.getName().equals(eName.toLowerCase())).findAny().get();
+        return e;
     }
 
-    /**
-     * @param args
-     * @throws Exception
-     * @author Azra Žunić
-     */
-    public static void main(String[] args) throws Exception {
-        Options options = addOptions();
-        CommandLineParser commandLineParser = new DefaultParser();
-        CommandLine cl = commandLineParser.parse(options, args);
-
-        //ovaj dio je za pacijenta i fk blood type
-      if ((cl.hasOption(addPatient.getOpt()) || cl.hasOption(addPatient.getLongOpt())) && cl.hasOption((bloodtype.getLongOpt()))) {
-           PatientManager patientManager = new PatientManager();
-           BloodManager bloodManager = new BloodManager();
-           Blood blood = null;
-             try {
-                blood = searchThroughBloodTypes(bloodManager.getAll(), cl.getArgList().get(1));
-            } catch (Exception e) {
-                System.out.println("There is no blood group in the list! Try again.");
-                System.exit(1);
-            }
-
-             //dodajem pacijenta u bazu
-            Patient patient = new Patient();
-             patient.setFk_BloodType(blood);
-            patient.setFull_Name(cl.getArgList().get(0));
-            patient.setDateOfBirth((LocalDate.of(2002,2,15)));
-            patientManager.add(patient);
-            System.out.println("You successfully added patient to database!");
-
-        } else if(cl.hasOption(getPatient.getOpt()) || cl.hasOption(getPatient.getLongOpt())){
-            PatientManager patientManager = new PatientManager();
-            patientManager.getAll().forEach(q -> System.out.println(q.getFull_Name()));
-
-        } else if(cl.hasOption(addBlood.getOpt()) || cl.hasOption(addBlood.getLongOpt())){
-            try {
-                BloodManager bloodManager = new BloodManager();
-                Blood b = new Blood();
-                b.setBloodGroup(cl.getArgList().get(0));
-                bloodManager.add(b);
-                System.out.println("Blood group has been added successfully");
-
-            }catch(Exception e) {
-                System.out.println("There is already the same blood group in database! Try again");
-                System.exit(1);
-            }
-
-        } else if(cl.hasOption(getBlood.getOpt()) || cl.hasOption(getBlood.getLongOpt())){
-          BloodManager bloodManager = new BloodManager();
-          bloodManager.getAll().forEach(c -> System.out.println(c.getBloodGroup()));
-
-        } else {
-            printFormattedOptions(options);
-            System.exit(-1);
-
-        }
-    }
 }
 
 
